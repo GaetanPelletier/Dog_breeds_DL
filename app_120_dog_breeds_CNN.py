492131824a49b2123697fe663bddb8e3618d2355
GaetanPelletier <76438369+GaetanPelletier@users.noreply.github.com>
2021-04-20 10:30:42 +0200

    Add files via upload

---
+++ app_120_dog_breeds_CNN.py
@@ -0,0 +1,51 @@
+import tensorflow.keras as keras
+import gradio
+
+#------------------#
+
+# Load the model
+model_xception_120_dog_breeds = keras.models.load_model("Model/transfert_learning_xception_full_19042021.h5")
+
+#Load the labels
+labels_120_dog_breeds = []
+with open("Labels/labels_120_dog_breeds.txt", "r") as f:
+  for line in f:
+    labels_120_dog_breeds.append(line.strip())
+
+# Image examples
+doberman = ["Image_examples/n02107142_14066.jpg"]
+labrador = ["Image_examples/n02099712_610.jpg"]
+husky = ["Image_examples/n02110185_11626.jpg"]
+
+#------------------#
+
+# Function for preprocessing an image and predicting the dog breed
+def classify_image(image_):
+  img = image_.reshape((-1, 299, 299, 3))
+  img = keras.applications.xception.preprocess_input(img)
+  
+  prediction = model_xception_120_dog_breeds.predict(img).flatten()
+
+  return {labels_120_dog_breeds[i]: float(prediction[i]) for i in range(120)}
+
+# Define the inputs, outputs and examples
+image = gradio.inputs.Image(shape=(299, 299))
+label = gradio.outputs.Label(num_top_classes=3)
+
+sample_images = [
+  doberman,
+  labrador,
+  husky
+]
+
+#------------------#
+
+# Launch the application
+gradio.Interface(
+    fn=classify_image,
+    inputs=image,
+    outputs=label,
+    title="CNN for 120 dog breeds classification",
+    description="This CNN was built thanks to transfer learning (Xception, F. Chollet).",
+    examples=sample_images
+).launch(debug=False)
